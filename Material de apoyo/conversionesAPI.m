357ec9b52a1f7ed049f0f25ae995fda670e10457
Santiago Burgos <jsbm1089@gmail.com>
2015-04-23 21:46:48 -0600

    Añadido programa de conversion entre medidas varias en la matematica, se pueden ir agregando mas conversiones con el formato obligado ida-vuelta en cada case dependiendo de los calculos

---
+++ Material de apoyo/conversionesAPI.m
@@ -0,0 +1,155 @@
+%Conversiones varias
+%Aca se dan varias medidas de conversion para medidas varias
+
+%1 plg ~ 2.54 cm (OK)
+%1 cm ~ 0.3937007874 plg (OK)
+%1 ft ~ 0.3048 m (OK)
+%1 m ~ 3.280839895 ft (OK)
+%1 milla ~ 1.609344 km (OK)
+%1 km ~ 0.6213711922 millas (OK)
+%1 lb ~ 0.4535924 kg (OK)
+%1 kg ~ 2.204622476 lb (OK)
+%1 J ~ 0.2389029576 cal (OK)
+%1 cal ~ 4.1858 J (OK)
+%1 oz ~ 28.34952 g (OK)
+%1 g ~ 0.03527396584 oz (OK)
+%1 ft ~ 12 plg (OK)
+%1 plg ~ 0.08333 ft (OK)
+
+% Ingresar el valor a convertir
+% Ingresar la medición a convertir
+% Ingresar la opción de conversión de datos
+% El programa retorna el valor convertido en la unidad deseada
+function [output] = conversionesAPI(input, option)
+
+format long
+fprintf('Conversion de valores para mediciones varias MATLAB \n\n');
+
+switch option
+
+	% pulgadas -> cm
+    case 1
+        fprintf('El valor ingresado es: %2.15f pulgadas \n',input);
+        output = input*2.540;
+        fprintf('El valor de salida es : %2.15f cm \n',output);
+    
+    % cm -> pulgadas   
+    case 2
+        fprintf('El valor ingresado es: %2.15f cm \n',input);
+        output = input*0.3937007874;
+        fprintf('El valor de salida es : %2.15f pulgadas \n',output);
+        
+    % ft -> m
+    case 3
+        fprintf('El valor ingresado es: %2.15f ft \n',input);
+        output = input*0.3048;
+        fprintf('El valor de salida es : %2.15f m \n',output);
+        
+    % m -> ft
+    case 4
+        fprintf('El valor ingresado es: %2.15f m \n',input);
+        output = input*3.280839895;
+        fprintf('El valor de salida es : %2.15f ft \n',output);
+
+	% millas -> km
+	case 5
+        fprintf('El valor ingresado es: %2.15f millas \n',input);
+        output = input*1.609344;
+        fprintf('El valor de salida es : %2.15f km \n',output);
+        
+    % km -> millas
+    case 6
+        fprintf('El valor ingresado es: %2.15f km \n',input);
+        output = input*0.6213711922;
+        fprintf('El valor de salida es : %2.15f millas \n',output);
+	
+	% lb -> kg
+	case 7
+        fprintf('El valor ingresado es: %2.15f lb \n',input);
+        output = input*0.4535924;
+        fprintf('El valor de salida es : %2.15f kg \n',output);
+        
+    % kg -> lb
+    case 8
+        fprintf('El valor ingresado es: %2.15f kg \n',input);
+        output = input*2.204622476;
+        fprintf('El valor de salida es : %2.15f lb \n',output);        
+
+	% J -> cal
+	case 9
+        fprintf('El valor ingresado es: %2.15f J \n',input);
+        output = input*0.2389029576;
+        fprintf('El valor de salida es : %2.15f cal \n',output);
+        
+    % cal -> J
+    case 10
+        fprintf('El valor ingresado es: %2.15f cal \n',input);
+        output = input*4.1858;
+        fprintf('El valor de salida es : %2.15f J \n',output); 
+
+	% oz -> g
+	case 11
+        fprintf('El valor ingresado es: %2.15f oz \n',input);
+        output = input*28.34952;
+        fprintf('El valor de salida es : %2.15f g \n',output);
+        
+    % g -> oz
+    case 12
+        fprintf('El valor ingresado es: %2.15f g \n',input);
+        output = input*0.03527396584;
+        fprintf('El valor de salida es : %2.15f oz \n',output);
+
+	% ft -> pulgadas
+	case 13
+        fprintf('El valor ingresado es: %2.15f ft \n',input);
+        output = input*12;
+        fprintf('El valor de salida es : %2.15f pulgadas \n',output);
+        
+    % pulgadas -> ft
+    case 14
+        fprintf('El valor ingresado es: %2.15f pulgadas \n',input);
+        output = input*0.08333;
+        fprintf('El valor de salida es : %2.15f ft \n',output);
+
+	% °C -> °F
+	case 15
+        fprintf('El valor ingresado es: %2.15f °C \n',input);
+        output = input*(9/5) + 32;
+        fprintf('El valor de salida es : %2.15f °F \n',output);
+        
+    % °F -> °C
+    case 16
+        fprintf('El valor ingresado es: %2.15f °F \n',input);
+        output = (input - 32)*(5/9);
+        fprintf('El valor de salida es : %2.15f °C \n',output);        
+
+	% °C -> °K
+	case 17
+        fprintf('El valor ingresado es: %2.15f °C \n',input);
+        output = input + 273.15;
+        fprintf('El valor de salida es : %2.15f °K \n',output);
+        
+    % °K -> °C
+    case 18
+        fprintf('El valor ingresado es: %2.15f °K \n',input);
+        output = input - 273.15;
+        fprintf('El valor de salida es : %2.15f °C \n',output);  
+
+ 	% °F -> °K
+ 	case 19
+        fprintf('El valor ingresado es: %2.15f °F \n',input);
+        input1 = (input - 32)*(5/9);
+        output = input1 + 273.15;
+        fprintf('El valor de salida es : %2.15f °K \n',output);
+        
+    % °K -> °F
+    case 20
+        fprintf('El valor ingresado es: %2.15f °K \n',input);
+        input1 = input*(9/5) + 32; 
+        output = input - 273.15;
+        fprintf('El valor de salida es : %2.15f °F \n',output);
+
+    otherwise
+        disp('Opción no disponible, favor colocar la opción adecuada')
+        
+end
