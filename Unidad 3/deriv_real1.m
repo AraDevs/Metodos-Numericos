aa13b4042e7bfa5914eef352f7e4114d9609d1ed
Santiago Burgos <jsbm1089@gmail.com>
2015-05-02 22:29:36 -0600

    renombrado archivo deriv_real.m a deriv_real1.m con los siguientes datos: uso del programa, datos a ocupar y motivos de estos, patch sobre advertencia y resultados de salida. Este archivo lo que realiza es obtener la n-derivada solicitada

---
+++ Unidad 3/deriv_real1.m
@@ -0,0 +1,38 @@
+%Derivada real
+%El uso de esta derivada está limitada a ejercicios que den una función,
+%caso contrario no es posible obtener la derivada real
+
+%Uso del programa
+%1) Llamar a la función como deriv_real('[funcion]',[n derivada]). 
+%2) ¡¡Importante que "funcion" vaya entre comillas!! De lo contrario el programa arrojará error.
+%3) Ingresar los valores de evaluación X en forma de vector.
+%4) Una vez ingresados todos estos datos procede a operar.
+
+% Este programa de la derivada real es un archivo de tipo function, lo cual permitirá
+% usarlo como una API para llamarlo en los métodos de derivadas aproximadas y que pueda
+% mostrar ambas soluciones requeridas: la derivada real y la derivada aproximada.
+% Si el ejercicio lo solicita se podrá obtener el error entre ambos datos.
+function [output] = deriv_real(fun, fd)
+
+format long
+disp('Derivada real')
+
+% Patch que permite remover advertencias del programa a efectuarse, esto permitirá
+% ocultar mensaje de advertencia sobre método char/diff a removerse en futuras versiones
+% Mensaje encontrado en Matlab R2011b, no testeado en otras versiones:
+% Warning: The method char/diff will be removed in a future release. Use sym/diff instead
+warning('off','all')
+
+% Ingreso de los valores del vector X (se ocupa X en mayúsculas para evitar conflictos)
+X = input('Ingrese los valores de X en formato [x0;x1;x2;...;xn]: ');
+
+% Fun(n) = diff([función],[derivada n])
+F = diff(fun,fd);
+
+% f(fd)(x) = subs(Fun(n),vector X(1:end-fd))
+%output = subs(F,X(1:end-fd));
+output = subs(F,X);
+
+% Imprime los resultados de la derivada solicitada
+fprintf('\n')
+fprintf('El valor de la derivada real es: \n');
