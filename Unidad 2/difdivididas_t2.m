22c083e6b1e6ed051a624d7a564a509936af76bd
Santiago Burgos <jsbm1089@gmail.com>
2016-03-03 16:56:53 -0600

    reescritos los programas de interpolacion por medio de Diferencias Divididas, los cuales llevan documentacion en los codigos, se deja mas entendible el funcionamiento y se le ha agregado la funcionalidad de mostrar las formulas de diferencias en conjunto con el polinomio resultante de Diferencias Divididas

--- Unidad 2/difdivididas_t2.m
+++ Unidad 2/difdivididas_t2.m
@@ -1,15 +1,17 @@
 %Metodo de Interpolacion por medio de Diferencias Divididas Version 2
-%1) Ingresar los valores de X dados en alguna tabla en particular
-%2) Ingresar los valores de Y respectivos de la misma tabla
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
-disp('Interpolacion por Diferencias Divididas V2')
-X = input('Ingrese los valores de X en formato [x0,x1,x2,...,xn]: ');
-Y = input('Ingrese los valores de Y en formato [y0,y1,y2,...,yn]: ');
+
+%1) Ingresar los valores de evaluación en formato vector
+%2) Ingresar los valores resultantes en formato vector
+%3) Ingresar el valor a aproximar
+disp('Interpolacion de Diferencias Divididas V2')
+X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
+Y = input('Ingrese los valores de y en formato [y0,y1,y2,...,yn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
 
@@ -18,22 +20,40 @@ n = length(X);
 MPol = zeros(n,n);
 MPol(:,1) = Y'
 
+% Se trabajaran los valores de numerador y denominador por aparte,
+% posteriormente se uniran estos valores para obtener el resultado
 for i=2:n
 	for j=i:n
-		MPol(j,i) = MPol(j,i-1) - MPol(j-1,i-1);
-		MPol(j,i) = (MPol(j,i))/(X(j) - X(j-i+1))
+		% num = F[j,i-1] - F[j-1,i-1]
+		num = MPol(j,i-1) - MPol(j-1,i-1);
+		% den = X(j) - X(j-i+1)
+		den = X(j) - X(j-i+1);
+		MPol(j,i) = num/den;
+
+		% Codigo patch para mostrar las formulas en cada iteracion
+		% sprintf permite mostrar el codigo en formato similar a como se escribiria a mano,
+		% solo que en este caso no soporta la funcion pretty
+		equ1 = 'F(%d,%d)=F(%d,%d) - F(%d,%d))/(x%d - x%d)';
+		sprintf(equ1,j,i,j,i-1,j-1,i-1,j,j-i+1)
+		equ2 = 'F(%d,%d)=%0.15f';
+		sprintf(equ2,j,i,MPol(j,i))
+
 	end
 end
 
+% Se inicializa el valor del polinomio aproximado a 0 y 1,
+% esto permite aplicar los factores suma y producto
+% del polinomio de Diferencias que se genera
 init = 0;
 PPol = 1;
 for i=1:n
 	PPol = PPol*(x - X(i)); %Factor de multiplicacion
-    FPol = init + MPol(i,i)*PPol; %Factor de suma
-    init = FPol;
+    Pol_Lag = init + MPol(i,i)*PPol; %Factor de suma
+    init = Pol_Lag;
 end
+pretty(Pol_Lag) %Polinomio de Diferencias
 
-pretty(FPol);
-R = subs(FPol,aprox)
+% Valor aproximado de la funcion
+ValA = subs(Pol_Lag,aprox);
 fprintf('\n');,
-fprintf('El valor aproximado de la funcion es: %2.15f \n\n', R);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
