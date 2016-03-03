0ab0e06712bc015cb1d3b0818708b158d54a33b4
Santiago Burgos <jsbm1089@gmail.com>
2016-03-03 14:52:47 -0600

    reescritos los programas de interpolacion por medio de Neville, los cuales llevan documentacion en los codigos, se deja mas entendible el funcionamiento y se le ha agregado la funcionalidad de mostrar la formula de donde sale el valor de la diagonal

--- Unidad 2/neville_t1.m
+++ Unidad 2/neville_t1.m
@@ -1,15 +1,18 @@
 %Metodo de Interpolacion por medio de Neville Version 1
-%1) Ingresar la funcion a evaluar junto con sus valores de evaluacion
-%2) Genera los valores de f(x) para cada uno de los valores ingresados
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
+
+%1) Ingresar la funcion a evaluar 
+%2) Ingresar los valores de evaluación en formato vector
+%3) Ingresar el valor a aproximar
+%4) Genera los valores de f(x) para cada uno de los valores del vector
 disp('Interpolacion de Neville V1')
 g = input('Ingrese la funcion a evaluar: ');
-X = input('Ingrese los valores de X en formato [x0,x1,x2,...,xn]: ');
+X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
 
@@ -20,20 +23,37 @@ for i=1:n
 	MPol(i,1) = subs(g,X(i));	
 end
 
+% Se trabajaran los valores de numerador y denominador por aparte,
+% posteriormente se uniran estos valores para obtener el resultado
 for i=2:n
 	for j=i:n
-		MPol(j,i) = (aprox - X(j-1))*MPol(j-1,i-1) - (aprox - X(j))*MPol(j,i-1);
-		MPol(j,i) = MPol(j,i)/(X(j) - X(j-i+1))
+		% num = (x - X(j-1))*Q(j-1,i-1) - (x - X(j))*Q(j,i-1)
+		num = (aprox - X(j-1))*MPol(j-1,i-1) - (aprox - X(j))*MPol(j,i-1);
+		% den = (X(j) - X(j-i+1))
+		den = X(j) - X(j-i+1);
+		MPol(j,i) = num/den;
+
+		% Codigo patch para mostrar las formulas en cada iteracion
+		% sprintf permite mostrar el codigo en formato similar a como se escribiria a mano,
+		% solo que en este caso no soporta la funcion pretty
+		equ1 = 'Q(%d,%d)=((x - x%d)Q(%d,%d) - (x - x%d)Q(%d,%d))/(x%d - x%d)';
+		sprintf(equ1,j,i,j-1,j-1,i-1,j,j,i-1,j,j-i+1)
+		equ2 = 'Q(%d,%d)=%0.15f';
+		sprintf(equ2,j,i,MPol(j,i))
+		
 	end
 end
 
-R = MPol(n,n);
+% Valor aproximado de la funcion
+ValA = MPol(n,n);
 fprintf('\n');,
-fprintf('El valor aproximado de la funcion es: %2.15f \n\n', R);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
 
+% Evaluacion del valor aproximado en la funcion real
 fprintf('\n');,
-real = subs(g,aprox);
-fprintf('El valor exacto de la funcion es: %2.15f \n\n', real);
+ValR = subs(g,aprox);
+fprintf('El valor exacto de la funcion es: %2.15f \n\n', ValR);
 
-raprox = abs(R - real);
-fprintf('El error de aproximacion es: %e \n', raprox);
+% Error de aproximacion
+tol = abs(ValA - ValR);
+fprintf('El error de aproximacion es: %e \n', tol);
--- Unidad 2/neville_t2.m
+++ Unidad 2/neville_t2.m
@@ -1,30 +1,47 @@
 %Metodo de Interpolacion por medio de Neville Version 2
-%1) Ingresar los valores de X dados en alguna tabla en particular
-%2) Ingresar los valores de Y respectivos de la misma tabla
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
+
+%1) Ingresar los valores de evaluación en formato vector
+%2) Ingresar los valores resultantes en formato vector
+%3) Ingresar el valor a aproximar
 disp('Interpolacion de Neville V2')
-X = input('Ingrese los valores de X en formato [x0,x1,x2,...,xn]: ');
-Y = input('Ingrese los valores de Y en formato [y0,y1,y2,...,yn]: ');
+X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
+Y = input('Ingrese los valores de y en formato [y0,y1,y2,...,yn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
 
 %matriz(filas,columnas)
 %matriz(arriba/abajo, derecha/izquierda)
 MPol = zeros(n,n);
-MPol(:,1) = Y'
+MPol(:,1) = Y';
 
+% Se trabajaran los valores de numerador y denominador por aparte,
+% posteriormente se uniran estos valores para obtener el resultado
 for i=2:n
 	for j=i:n
-		MPol(j,i) = (aprox - X(j-1))*MPol(j-1,i-1) - (aprox - X(j))*MPol(j,i-1);
-		MPol(j,i) = MPol(j,i)/(X(j) - X(j-i+1))
+		% num = (x - X(j-1))*Q(j-1,i-1) - (x - X(j))*Q(j,i-1)
+		num = (aprox - X(j-1))*MPol(j-1,i-1) - (aprox - X(j))*MPol(j,i-1);
+		% den = (X(j) - X(j-i+1))
+		den = X(j) - X(j-i+1);
+		MPol(j,i) = num/den
+
+		% Codigo patch para mostrar las formulas en cada iteracion
+		% sprintf permite mostrar el codigo en formato similar a como se escribiria a mano,
+		% solo que en este caso no soporta la funcion pretty
+		equ1 = 'Q(%d,%d)=((x - x%d)Q(%d,%d) - (x - x%d)Q(%d,%d))/(x%d - x%d)';
+		sprintf(equ1,j,i,j-1,j-1,i-1,j,j,i-1,j,j-i+1)
+		equ2 = 'Q(%d,%d)=%0.15f';
+		sprintf(equ2,j,i,MPol(j,i))
+		
 	end
 end
 
-R = MPol(n,n);
+% Valor aproximado de la funcion
+ValA = MPol(n,n);
 fprintf('\n');,
-fprintf('El valor aproximado de la funcion es: %2.15f \n\n', R);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
