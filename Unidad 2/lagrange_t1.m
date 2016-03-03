f2c21a61211282ce2959fcb84fcbe15c7619e30c
Sam Burgos <jsbm1089@gmail.com>
2016-03-02 21:40:35 -0600

    reescritos los programas de interpolacion por medio de Lagrange, los cuales llevan documentacion en los codigos y se dejado mas entendible su funcionamiento

--- Unidad 2/lagrange_t1.m
+++ Unidad 2/lagrange_t1.m
@@ -1,55 +1,59 @@
 %Metodo de Interpolacion por medio de Lagrange Version 1
-%1) Ingresar la funcion a evaluar junto con sus valores de evaluación
-%2) Genera los valores de f(x) para cada uno de los valores ingresados
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
+
+%1) Ingresar la funcion a evaluar 
+%2) Ingresar los valores de evaluación en formato vector
+%3) Ingresar el valor a aproximar
+%4) Genera los valores de f(x) para cada uno de los valores del vector
 disp('Interpolacion de Lagrange V1')
 g = input('Ingrese la funcion a evaluar: ');
 X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
 
-%matriz(filas,columnas)
-%matriz(arriba/abajo, derecha/izquierda)
-Y = zeros(n,n);
 for i=1:n
-	Y(i,1) = subs(g,X(i));	
+	Y(i) = subs(g,X(i));
 end
 
+% Se inicializan valores de numerador y denominador a 1,
+% esto permite aplicar el factor producto de Lagrange
 num=1;
 den=1;
-for i=1:n
-	if(i==1)
-		%(x - x0)
-		num = num*(x - X(2));
-		%(x0 - x1)
-		den = den*(X(1) - X(2));
-		L(i) = num/den;
-		pretty(L(i))
-		fprintf('\n');
-	else
-		%prod from {i=1} to {n} (x - x_i)
-		num=num*(x - X(i));
-        %prod from {i=1} to {n} (x_{i-1} - x_i)
-		den=den*(X(i-1) - X(i));
-		L(i) = num/den;
-		pretty(L(i))
-		fprintf('\n');
-	end	
+
+for i=2:n
+	%prod from {i=1} to {n} (x - x_i)
+	num=num*(x - X(i));
+    %prod from {i=1} to {n} (x_{i-1} - x_i)
+	den=den*(X(i-1) - X(i));
+	L(i) = num/den;
+	%pretty(L(i))
+	fprintf('\n');	
 end
 
+% Se inicializa el valor del polinomio aproximado a 0,
+% esto permite aplicar el factor suma del polinomio de Lagrange
 init = 0;
 for i=1:n
-    FPol = init + Y(i)*L(i);
-    init = FPol;
+    Pol_Lag = init + Y(i)*L(i);
+    init = Pol_Lag;
 end
-FPol;
-APol = subs(FPol,aprox);
-APol
-APol1 = subs(g,aprox)
-error = abs(APol1-APol);
-error = error * 100
+
+% Polinomio de Lagrange y valor aproximado
+disp('Polinomio de Lagrange resultante: ')
+fprintf('\n');
+pretty(Pol_Lag)
+ValA = subs(Pol_Lag,aprox);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
+
+% Evaluacion del valor aproximado en la funcion real
+fprintf('\n');,
+ValR = subs(g,aprox);
+fprintf('El valor exacto de la funcion es: %2.15f \n\n', ValR);
+
+tol = abs(ValA - ValR);
+fprintf('El error de aproximacion es: %e \n', tol);
--- Unidad 2/lagrange_t2.m
+++ Unidad 2/lagrange_t2.m
@@ -1,43 +1,48 @@
 %Metodo de Interpolacion por medio de Lagrange Version 2
-%1) Ingresar los valores de X dados en alguna tabla en particular
-%2) Ingresar los valores de Y respectivos de la misma tabla
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
+
+%1) Ingresar los valores de evaluación en formato vector
+%2) Ingresar los valores resultantes en formato vector
+%3) Ingresar el valor a aproximar
 disp('Interpolacion de Lagrange V2')
-X = input('Ingrese los valores de X en formato [x0,x1,x2,...,xn]: ');
+X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
 Y = input('Ingrese los valores de Y en formato [y0,y1,y2,...,yn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
 
-%matriz(filas,columnas)
-%matriz(arriba/abajo, derecha/izquierda)
-MPol = zeros(n,n);
-MPol(:,1) = Y'
-for i=1:n
-	Y(i) = MPol(i);
+% Se inicializan valores de numerador y denominador a 1,
+% esto permite aplicar el factor producto de Lagrange
+num=1;
+den=1;
+
+for i=2:n
+	%prod from {i=1} to {n} (x - x_i)
+	num=num*(x - X(i));
+    %prod from {i=1} to {n} (x_{i-1} - x_i)
+	den=den*(X(i-1) - X(i));
+	L(i) = num/den;
+	%pretty(L(i))
+	fprintf('\n');	
 end
 
-for i=1:n
-	if(i==1)
-		L(i) = ((x - X(2))/(X(1) - X(2)))
-		pretty(L(i))
-		fprintf('\n');
-	else
-		L(i) = ((x - X(i))/(X(i-1) - X(i)))
-		pretty(L(i))
-		fprintf('\n');
-	end	
-end
-
+% Se inicializa el valor del polinomio aproximado a 0,
+% esto permite aplicar el factor suma del polinomio de Lagrange
 init = 0;
 for i=1:n
-    FPol = init + Y(i)*L(i);
-    init = FPol;
+    Pol_Lag = init + Y(i)*L(i);
+    init = Pol_Lag;
 end
-FPol;
-APol = subs(FPol,aprox);
-APol
+
+% Polinomio de Lagrange y valor aproximado
+% Debido a que este modelo no coloca funcion real, no solicita
+% valor real ni error en valores
+disp('Polinomio de Lagrange resultante: ')
+fprintf('\n');
+pretty(Pol_Lag)
+ValA = subs(Pol_Lag,aprox);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
