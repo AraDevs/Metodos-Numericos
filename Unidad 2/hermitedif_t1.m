c44d11e773808ef9c6f48918e49acee984af75ab
Sam Burgos <jsbm1089@gmail.com>
2016-03-03 22:37:25 -0600

    reescritos los programas de interpolacion por medio de Hermite por Diferencias Divididas, los cuales llevan documentacion en los codigos, se deja mas entendible el funcionamiento y se le ha agregado la funcionalidad de mostrar las formulas de diferencias en conjunto co el polinomio resultante de Hermite

--- Unidad 2/hermitedif_t1.m
+++ Unidad 2/hermitedif_t1.m
@@ -1,72 +1,93 @@
-%Metodo de Interpolacion por medio de Diferencias Divididas Version 1
-%1) Ingresar la funcion a evaluar junto con sus valores de evaluación
-%2) Genera los valores de f(x) para cada uno de los valores ingresados
-%3) Genera los valores de f'(x) para cada uno de los valores ingresados
+%Metodo de Interpolacion por medio de Hermite por Diferencias Version 1
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
-disp('Interpolacion de Hermite mediante Diferencias Divididas V1')
+
+%1) Ingresar la funcion a evaluar 
+%2) Ingresar los valores de evaluación en formato vector
+%3) Ingresar el valor a aproximar
+%4) Genera la derivada de la funcion ingresada
+%5) La columna que genera es de tamaño doble ya que se duplican los valores ingresados
+%6) Se asignaran los valores de la columna de la matriz normal a la matriz doble
+disp('Interpolacion de Hermite por Diferencias Divididas V1')
 g = input('Ingrese la funcion a evaluar: ');
-X = input('Ingrese los valores de X en formato [x0,x1,x2,...,xn]: ');
+X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
-np = 2*n; %se realizara una columna de tamaño doble
 G = diff(g,x);
 
+% Seccion de asignacion de valores
 %matriz(filas,columnas)
 %matriz(arriba/abajo, derecha/izquierda)
-MPol = zeros(np,np);
-
-%En este metodo los valores se repiten 2 veces por cada columna
-%Se asignaran los valores de la columna de la matriz normal a la matriz doble
+MPol = zeros(2*n,2*n);
 for i=1:n
-	%MPol(2*i) = Y(i);
+	% Asignacion de valores evaluados en la funcion
 	MPol(2*i) = subs(g,X(i));
-	%MPol(2*i-1) = Y(i);
 	MPol(2*i-1) = subs(g,X(i));
 	
+	% Conversion del vector x en formato doble
 	Xm(2*i) = X(i);
 	Xm(2*i-1) = X(i);
 	
-	%Ydm(2*i) = Yd(i);
-	Ydm(2*i) = subs(G,X(i));
-	%Ydm(2*i-1) = Yd(i);
-	Ydm(2*i-1) = subs(G,X(i));
+	% Asignacion de valores evaluados en la derivada
+	Z(2*i) = subs(G,X(i));
+	Z(2*i-1) = subs(G,X(i));
 end
 
-for i=2:np
-	for j=i:np
-		MPol(j,i) = MPol(j,i-1) - MPol(j-1,i-1);
-		num = MPol(j,i);
-		den = (Xm(j) - Xm(j-i+1));
+% Se trabajaran los valores de numerador y denominador por aparte,
+% posteriormente se uniran estos valores para obtener el resultado
+for i=2:2*n
+	for j=i:2*n
+		%num = F[j,i-1] - F[j-1,i-1]
+		num = MPol(j,i-1) - MPol(j-1,i-1);
+		%den = X(j) - X(j-i+1)
+		den = Xm(j) - Xm(j-i+1);
 		
+		% El valor de la derivada solo debe aparecer en la 1a corrida,
+		% a partir de la 2a corrida en adelante esta no debe estar presente
 		if(i==2 & den==0)
-			MPol(j,i) = Ydm(j)
+			MPol(j,i) = Z(j);
 		else
-			MPol(j,i) = num/den
+			MPol(j,i) = num/den;
 		end
+
+		% Codigo patch para mostrar las formulas en cada iteracion
+		% sprintf permite mostrar el codigo en formato similar a como se escribiria a mano,
+		% solo que en este caso no soporta la funcion pretty
+		equ1 = 'F(%d,%d)=F(%d,%d) - F(%d,%d))/(x%d - x%d)';
+		sprintf(equ1,j,i,j,i-1,j-1,i-1,j,j-i+1)
+		equ2 = 'F(%d,%d)=%0.15f';
+		sprintf(equ2,j,i,MPol(j,i))
+
 	end
 end
 
+% Se inicializa el valor del polinomio aproximado a 0 y 1,
+% esto permite aplicar los factores suma y producto
+% del polinomio de Hermite que se genera
 init = 0;
 PPol = 1;
-for i=1:np-1
+for i=1:(2*n-1)
 	PPol = PPol*(x - Xm(i)); %Factor de multiplicacion
-    FPol = init + MPol(i,i)*PPol; %Factor de suma
-    init = FPol;
+    Pol_Herm = init + MPol(i,i)*PPol; %Factor de suma
+    init = Pol_Herm;
 end
 
-pretty(FPol);
-R = subs(FPol,aprox)
-fprintf('\n');,
-fprintf('El valor aproximado de la funcion es: %2.15f \n\n', R);
+pretty(Pol_Herm) %Polinomio de Hermite por Diferencias
 
+% Valor aproximado de la funcion
+ValA = subs(Pol_Herm,aprox);
 fprintf('\n');,
-real = subs(g,aprox);
-fprintf('El valor exacto de la funcion es: %2.15f \n\n', real);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
 
-raprox = abs(R - real);
-fprintf('El error de aproximacion es: %e \n', raprox);
+% Evaluacion del valor aproximado en la funcion real
+fprintf('\n');,
+ValR = subs(g,aprox);
+fprintf('El valor exacto de la funcion es: %2.15f \n\n', ValR);
+
+% Error de aproximacion
+tol = abs(ValA - ValR);
+fprintf('El error de aproximacion es: %e \n', tol);
--- Unidad 2/hermitedif_t2.m
+++ Unidad 2/hermitedif_t2.m
@@ -1,89 +1,84 @@
-%Metodo de Interpolacion por medio de Diferencias Divididas Version 2
-%1) Ingresar los valores de X dados en alguna tabla en particular
-%2) Ingresar los valores de Y respectivos de la misma tabla
-%3) Ingresar los valores de las derivadas otorgadas
+%Metodo de Interpolacion por medio de Hermite por Diferencias Version 2
+
+% Seccion de inicializacion
 clear all
 clc
 syms x
-%format long permite colocar los numeros con 15 decimales
-%dicha cantidad es la mas ocupada en este tipo de ejercicios
 format long
+
+%1) Ingresar los valores de evaluación x en formato vector 
+%2) Ingresar los valores de evaluación y en formato vector
+%3) Ingresar los valores de la derivada en formato vector
+%4) Genera la derivada de la funcion ingresada
+%5) La columna que genera es de tamaño doble ya que se duplican los valores ingresados
+%6) Se asignaran los valores de la columna de la matriz normal a la matriz doble
 disp('Interpolacion de Hermite mediante Diferencias Divididas V2')
 X = input('Ingrese los valores de X en formato [x0,x1,x2,...,xn]: ');
 Y = input('Ingrese los valores de Y en formato [y0,y1,y2,...,yn]: ');
 Yd = input('Ingrese los valores de las derivadas en formato [yd0,yd1,yd2,...,ydn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
 n = length(X);
-np = 2*n; %se realizara una columna de tamaño doble
 
+% Seccion de asignacion de valores
 %matriz(filas,columnas)
 %matriz(arriba/abajo, derecha/izquierda)
-MPol = zeros(np,np);
-
-%En este metodo los valores se repiten 2 veces por cada columna
-%Se asignaran los valores de la columna de la matriz normal a la matriz doble
+MPol = zeros(2*n,2*n);
 for i=1:n
+	% Conversion del vector y en formato doble
 	MPol(2*i) = Y(i);
 	MPol(2*i-1) = Y(i);
 	
+	% Conversion del vector x en formato doble
 	Xm(2*i) = X(i);
 	Xm(2*i-1) = X(i);
 	
-	Ydm(2*i) = Yd(i);
-	Ydm(2*i-1) = Yd(i);
+	% Conversion del vector derivadas en formato doble
+	Z(2*i) = Yd(i);
+	Z(2*i-1) = Yd(i);
 end
 
-for i=2:np
-	for j=i:np
-		MPol(j,i) = MPol(j,i-1) - MPol(j-1,i-1);
-		num = MPol(j,i);
-		den = (Xm(j) - Xm(j-i+1));
+% Se trabajaran los valores de numerador y denominador por aparte,
+% posteriormente se uniran estos valores para obtener el resultado
+for i=2:2*n
+	for j=i:2*n
+		%num = F[j,i-1] - F[j-1,i-1]
+		num = MPol(j,i-1) - MPol(j-1,i-1);
+		%den = X(j) - X(j-i+1)
+		den = Xm(j) - Xm(j-i+1);
 		
+		% El valor de la derivada solo debe aparecer en la 1a corrida,
+		% a partir de la 2a corrida en adelante esta no debe estar presente
 		if(i==2 & den==0)
-			MPol(j,i) = Ydm(j)
+			MPol(j,i) = Z(j);
 		else
-			MPol(j,i) = num/den
+			MPol(j,i) = num/den;
 		end
+
+		% Codigo patch para mostrar las formulas en cada iteracion
+		% sprintf permite mostrar el codigo en formato similar a como se escribiria a mano,
+		% solo que en este caso no soporta la funcion pretty
+		equ1 = 'F(%d,%d)=F(%d,%d) - F(%d,%d))/(x%d - x%d)';
+		sprintf(equ1,j,i,j,i-1,j-1,i-1,j,j-i+1)
+		equ2 = 'F(%d,%d)=%0.15f';
+		sprintf(equ2,j,i,MPol(j,i))
+
 	end
 end
 
+% Se inicializa el valor del polinomio aproximado a 0 y 1,
+% esto permite aplicar los factores suma y producto
+% del polinomio de Hermite que se genera
 init = 0;
 PPol = 1;
-for i=1:np-1
+for i=1:(2*n-1)
 	PPol = PPol*(x - Xm(i)); %Factor de multiplicacion
-    FPol = init + MPol(i,i)*PPol; %Factor de suma
-    init = FPol;
+    Pol_Herm = init + MPol(i,i)*PPol; %Factor de suma
+    init = Pol_Herm;
 end
 
-pretty(FPol);
-R = subs(FPol,aprox)
+pretty(Pol_Herm) %Polinomio de Hermite por Diferencias
+
+% Valor aproximado de la funcion
+ValA = subs(Pol_Herm,aprox);
 fprintf('\n');,
-fprintf('El valor aproximado de la funcion es: %2.15f \n\n', R);
-%MPol = zeros(np,np)
-%MPol(:,1) = Yr'
-
-%for i=2:n
-%	for j=i:n
-%		MPol(j,i) = MPol(j,i-1) - MPol(j-1,i-1);
-%		MPol(j,i) = (MPol(j,i))/(X(j) - X(j-i+1))
-%	end
-%end
-
-%init = 0;
-%PPol = 1;
-%for i=1:n
-%	PPol = PPol*(x - X(i)); %Factor de multiplicacion
-%    FPol = init + MPol(i,i)*PPol; %Factor de suma
-%    init = FPol;
-%end
-
-%pretty(FPol);
-%R = subs(FPol,aprox)
-%fprintf('\n');,
-%fprintf('El valor aproximado de la funcion es: %2.15f \n\n', R);
-
-%real = subs(g,aprox);
-%fprintf('El valor exacto de la funcion es: %2.15f \n\n', real);
-
-%raprox = abs(R - real);
-%fprintf('El error de aproximacion es: %e \n', raprox);
+fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
