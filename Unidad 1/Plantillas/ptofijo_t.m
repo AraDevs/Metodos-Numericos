9c0e03102e5f11888353254f80f6ebc0e83b5306
Sam Burgos <jsbm1089@gmail.com>
2016-06-05 13:54:25 -0600

    actualizado método de Punto Fijo, se estandariza el valor de la variable de error, se corrige error inicial del valor de tolerancia y se define un nuevo método para impresión de resultados mas eficiente y ordenado que el que se tenía anteriormente

--- Unidad 1/Plantillas/ptofijo_t.m
+++ Unidad 1/Plantillas/ptofijo_t.m
@@ -9,27 +9,27 @@ disp('Metodo de Iteracion de Punto fijo')
 % Para la funcion de trabajo, esta debe estar en el formato f(x)=x
 g = input('Introduzca la funcion g: ');
 po = input('Introduzca el valor inicial: ');
-delta = input('Introduzca el valor del error: ');
+error = input('Introduzca el valor del error: ');
 
 % Seccion de evaluaciones del método (1/2)
 p1 = subs(g,po);
 cont = 1;
-z = abs(subs(g,po));
+tol = abs(p1 - po);
 
 
 % Mostrar los valores solicitados en pantalla
-disp('n		p0 		p1 		error')
-fprintf('%3.0f \t %2.15f \t %2.15f \t %e \n', cont, po, p1, z)
+fprintf('n \t\t p0 \t\t p1 \t \t error')
+fprintf('%3.0f \t %2.15f \t %2.15f \t %e \n',cont,po,p1,tol)
 
 % Seccion de corrida del metodo (2/2)
 % Verificacion: Mientras no cumpla el criterio de paro, la funcion debe seguir mostrando resultados
-while(abs(p1 - po) > delta)
+while(abs(p1 - po) > error)
     cont = cont + 1;
     po = p1;
     p1 = subs(g,po);
     tol = abs(p1 - po);
-    fprintf('%3.0f \t %2.15f \t %2.15f \t %e \n', cont, po, p1, tol) 
+    fprintf('%3.0f \t %2.15f \t %2.15f \t %e \n',cont,po,p1,tol)
 end
 
-% Mostrar respuesta en pantalla
+% Mostrar respuesta aproximada en pantalla
 fprintf('El valor aproximado de x es: %2.15f\n', p1);
