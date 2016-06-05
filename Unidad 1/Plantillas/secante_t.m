455e8f59f71b60cee7923e7779e25e9cd8274b70
Sam Burgos <jsbm1089@gmail.com>
2016-06-05 14:04:08 -0600

    actualizado método de la Secante, se estandariza el valor de la variable de error, se corrige error inicial del valor de tolerancia y se define un nuevo método para impresión de resultados mas eficiente y ordenado que el que se tenía anteriormente

--- Unidad 1/Plantillas/secante_t.m
+++ Unidad 1/Plantillas/secante_t.m
@@ -10,30 +10,31 @@ disp('Metodo de la Secante')
 g = input('introduzca la funcion g: ');
 po = input('Introduzca el valor inicial 1: ');
 p1 = input('Introduzca el valor inicial 2: ');
-delta = input('Introduzca el valor del error: ');
+error = input('Introduzca el valor del error: ');
 
 % Sección de evaluaciones del metodo (1/2)
 a1 = subs(g,p1);
 a2 = subs(g,po);
 pk = p1 - a1*(p1-po)/(a1 - a2);
 cont = 1;
-tol = abs(subs(g,pk));
+tol = abs(pk - p1);
 
 % Mostrar los valores solicitados en pantalla
-disp('n		p0		p1 		p2 		error')
-fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f 		%e\n', cont, po,p1,pk,tol);
+fprintf('n \t\t p0 \t\t p1 \t\t p2 \t\t error')
+fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %e\n',cont,po,p1,pk,tol)
 
 % Seccion de corrida del metodo (2/2)
 % Verificacion: Mientras no cumpla el criterio de paro, la funcion debe seguir mostrando resultados
-while(abs(pk-p1) > delta)
+while(abs(pk - p1) > error)
 	cont = cont + 1;
 	po = p1;
 	p1 = pk;
 
 	pk = p1 - a1*(p1-po)/(a1 - a2);
-	tol = abs(pk-p1);
-	fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f 		%e\n', cont, po,p1,pk,tol);
+	tol = abs(pk - p1);
+	fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %e\n',cont,po,p1,pk,tol);
 end
 
-% Mostrar respuesta en pantalla
-fprintf('El valor aproximado de x es: %2.15f\n', pk);
+% Mostrar respuesta aproximada en pantalla
+fprintf('\n')
+fprintf('El valor aproximado de x es: %2.15f\n', pk)
