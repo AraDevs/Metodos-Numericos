be676fa67013f18009d62a21dc59b074e0d17894
Sam Burgos <jsbm1089@gmail.com>
2016-06-05 15:06:34 -0600

    actualizado ejercicio 1 guia 1

--- Unidad 1/ejercicio1.m
+++ Unidad 1/ejercicio1.m
@@ -1,21 +1,19 @@
-% Sección de inicialización
+% Seccion de inicializacion
 clear all
 clc
 format long
 syms x
 disp('Ejercicio 1 Guia 1 MEN')
 
-% Sección de introducción de datos de trabajo
-% g = (log(17-3*x))/(log (9)) + x^2 - 20.820156997794982
-% po = 4, p1 = 5, fzero = 4.50
-% Se recomienda usar como valores de extremo valores entre 4.499 y 4.502,
-% con esto se reducirían las iteraciones necesarias
-g = input('introduzca la función g: ');
-po = input('Introduzca el valor inicial 1: ');
-p1 = input('Introduzca el valor inicial 2: ');
-delta = input('Introduzca el valor del error: ');
+% Sección de introduccion de datos de trabajo
+% Para la funcion de trabajo, esta debe estar en el formato f(x)=0
+% valor del solve: 6.1249999999999999653278473434029
+g = (log(17+5*x))/(log(15)) + x^2 - 38.942244725097574;
+po = 6.120;
+p1 = 6.124;
+error = 1e-12;
 
-% Sección de evaluaciones del método (1/2)
+% Sección de evaluaciones del metodo (1/2)
 a1 = subs(g,p1);
 a2 = subs(g,po);
 pk = p1 - a1*(p1-po)/(a1 - a2);
@@ -23,19 +21,22 @@ cont = 1;
 tol = abs(subs(g,pk));
 
 % Mostrar los valores solicitados en pantalla
-disp('n		p0		p1 		p2 		error')
-fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f 		%e\n', cont, po,p1,pk,tol);
+%disp('n		p0		p1 		p2 		error')
+fprintf('n \t\t p0 \t\t\t p1 \t\t\t p2 \t\t   error \n')
+fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %e\n', cont, po,p1,pk,tol)
 
-% Sección de corrida del método explicado (2/2)
-while(abs(pk-p1) > delta)
+% Seccion de corrida del metodo (2/2)
+% Verificacion: Mientras no cumpla el criterio de paro, la funcion debe seguir mostrando resultados
+while(abs(pk-p1) > error)
 	cont = cont + 1;
 	po = p1;
 	p1 = pk;
 
 	pk = p1 - a1*(p1-po)/(a1 - a2);
 	tol = abs(pk-p1);
-	fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f 		%e\n', cont, po,p1,pk,tol);
+	fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %e\n', cont, po,p1,pk,tol)
 end
 
-% Mostrar respuesta en pantalla
-fprintf('El valor aproximado de x es: %2.15f\n', pk);
+% Mostrar respuesta aproximada en pantalla
+fprintf('\n')
+fprintf('El valor aproximado de x es: %2.15f\n', pk)
