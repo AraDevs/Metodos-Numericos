35d4e0d8b8cda617349cf85f40b04d00a6a7c421
Sam Burgos <jsbm1089@gmail.com>
2016-06-05 14:08:46 -0600

    actualizado método de Posición Falsa, se corrige error que mostraba variable no definida, se estandariza variable de error y se define un nuevo método para impresión de resultados mas eficiente y ordenado que el que se tenía anteriormente

--- Unidad 1/Plantillas/posfalsa_t.m
+++ Unidad 1/Plantillas/posfalsa_t.m
@@ -10,7 +10,7 @@ disp('Metodo de la Posicion falsa')
 g = input('introduzca la funcion g: ');
 po = input('Introduzca el valor inicial 1: ');
 p1 = input('Introduzca el valor inicial 2: ');
-delta = input('Introduzca el valor del error: ');
+error = input('Introduzca el valor del error: ');
 
 % Seccion de evaluaciones del metodo (1/2)
 a1 = subs(g,p1);
@@ -22,30 +22,32 @@ if(a1*a2<0)
 	pk = p1 - a1*(p1-po)/(a1 - a2);
 	a3 = subs(g,pk);
 	cont = 1;
-	tol = abs(a3);
+	tol = abs(pk - p1);
 
-	disp('n		p0		p1 		p2 		error')
-	fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f 		%e\n', cont, po,p1,pk,tol);
+	fprintf('n \t\t p0 \t\t p1 \t\t p2 \t\t error')
+	fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %e\n',cont,po,p1,pk,tol)
 
-	while(abs(pk-p1) > delta)
+	while(abs(pk - p1) > error)
 	cont = cont + 1;
 
 	if(a1*a3 < 0)
 		po = p1;
 		p1 = pk;
 		pk = p1 - a1*(p1-po)/(a1 - a2);
-		tol = abs(pk-p1);
+		tol = abs(pk - p1);
 	else
 		po = po;
 		p1 = pk;
 		pk = p1 - a1*(p1-po)/(a1 - a2);
-		tol = abs(pk-p1);
+		tol = abs(pk - p1);
 	end
 
 	a3 = subs(g,pk);	
-	fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f 		%e\n', cont, po,p1,pk,tol);
+	fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %e\n',cont,po,p1,pk,tol)
 	end
-end
 
-% Mostrar respuesta en pantalla
-fprintf('El valor aproximado de x es: %2.15f\n', pk);
+	% Mostrar respuesta aproximada en pantalla
+	fprintf('\n')
+	fprintf('El valor aproximado de x es: %2.15f\n', pk)
+	
+end
