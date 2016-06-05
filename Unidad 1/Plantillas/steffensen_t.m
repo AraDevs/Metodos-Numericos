fd4d10317d27cd1b079b9616860d641e8e053536
Sam Burgos <jsbm1089@gmail.com>
2016-06-05 14:12:04 -0600

    actualizado método de Steffensen, se corrige error inicial del valor de tolerancia y se define un nuevo método para impresión de resultados mas eficiente y ordenado que el que se tenía anteriormente

--- Unidad 1/Plantillas/steffensen_t.m
+++ Unidad 1/Plantillas/steffensen_t.m
@@ -19,8 +19,8 @@ tol = abs(P - po);
 cont = 1;
 
 % Mostrar los valores solicitados en pantalla
-disp('n		P0		P1 		P2 		P		error')
-fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f		%2.15f 		%e\n', cont, po,p1,p2,P,tol);
+fprintf('n \t\t P0 \t\t P1 \t\t P2 \t\t P \t\t error')
+fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %2.15f \t %e\n',cont,po,p1,p2,P,tol);
 
 % Seccion de corrida del metodo (2/2)
 % Verificacion: Mientras no cumpla el criterio de paro, la funcion debe seguir mostrando resultados
@@ -32,8 +32,9 @@ while(abs(P - po) > error)
 	P = po - (p1 - po)^2/(p2 + 2*p1 + po);
 	tol = abs(P - po);
 
-	fprintf('%3.0f	%2.15f 		%2.15f 		%2.15f		%2.15f 		%e\n', cont, po,p1,p2,P,tol);
+	fprintf('%3.0f \t %2.15f \t %2.15f \t %2.15f \t %2.15f \t %e\n',cont,po,p1,p2,P,tol);
 end
 
-% Mostrar respuesta en pantalla
-fprintf('El valor aproximado de x es: %2.15f\n', P);
+% Mostrar respuesta aproximada en pantalla
+fprintf('\n')
+fprintf('El valor aproximado de x es: %2.15f\n', P)
