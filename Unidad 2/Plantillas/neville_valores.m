7d5ae6ee3c2fab3269ad3c5d8bd844dea16e81d9
Sam Burgos <jsbm1089@gmail.com>
2016-06-15 18:31:38 -0600

    actualizada plantilla de Neville que solicita valores de X y Y

--- Unidad 2/Plantillas/neville_valores.m
+++ Unidad 2/Plantillas/neville_valores.m
@@ -1,34 +1,42 @@
-%Metodo de Interpolacion por medio de Neville Version 2
+%Metodo de Interpolacion por Neville con valores
 
 % Seccion de inicializacion
 clear all
 clc
 syms x
 format long
+disp('Metodo de Interpolacion por Neville con valores')
 
-%1) Ingresar los valores de evaluación en formato vector
-%2) Ingresar los valores resultantes en formato vector
+% Seccion de introduccion de datos de trabajo
+%1) Ingresar la funcion a evaluar para obtener los valores generados
+%2) Ingresar los valores de evaluacion en formato vector
 %3) Ingresar el valor a aproximar
-disp('Interpolacion de Neville V2')
+%4) Se obtienen la cantidad de puntos para empezar a crear el polinomio
 X = input('Ingrese los valores de x en formato [x0,x1,x2,...,xn]: ');
 Y = input('Ingrese los valores de y en formato [y0,y1,y2,...,yn]: ');
 aprox = input('Ingrese el valor a aproximar: ');
-n = length(X);
+m = length(X);
 
-%matriz(filas,columnas)
-%matriz(arriba/abajo, derecha/izquierda)
-MPol = zeros(n,n);
-MPol(:,1) = Y';
+% Seccion de evaluacion de datos
+%5) Se generan los valores iniciales del polinomio en base a cada valor de X
+%6) Se genera un vector (llamado aquí MPol) para mostrar en pantalla y operar
+% matriz(filas,columnas)
+% matriz(arriba/abajo, derecha/izquierda)
+MPol = zeros(m,m);
+for i=1:m
+	MPol(i,1) = Y(i);	
+end
 
+% Seccion de corrida del metodo de interpolacion
 % Se trabajaran los valores de numerador y denominador por aparte,
 % posteriormente se uniran estos valores para obtener el resultado
-for i=2:n
-	for j=i:n
+for i=2:m
+	for j=i:m
 		% num = (x - X(j-1))*Q(j-1,i-1) - (x - X(j))*Q(j,i-1)
-		num = (aprox - X(j-1))*MPol(j-1,i-1) - (aprox - X(j))*MPol(j,i-1);
 		% den = (X(j) - X(j-i+1))
+		num = (aprox - X(j-1))*MPol(j,i-1) - (aprox - X(j))*MPol(j-1,i-1);
 		den = X(j) - X(j-i+1);
-		MPol(j,i) = num/den
+		MPol(j,i) = num/den;
 
 		% Codigo patch para mostrar las formulas en cada iteracion
 		% sprintf permite mostrar el codigo en formato similar a como se escribiria a mano,
@@ -41,7 +49,7 @@ for i=2:n
 	end
 end
 
-% Valor aproximado de la funcion
-ValA = MPol(n,n);
+% Valor aproximado de la funcion generada por Neville
+ValA = MPol(m,m);
 fprintf('\n');,
 fprintf('El valor aproximado de la funcion es: %2.15f \n\n', ValA);
