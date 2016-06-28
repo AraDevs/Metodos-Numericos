9d86fa62cffb2d3074741686e92efa3aea4a83f1
Sam Burgos <jsbm1089@gmail.com>
2016-06-28 16:57:05 -0600

    añadida plantilla de Método de Heun (Runge-Kutta)

---
+++ Unidad 4/Plantillas/RK_heun.m
@@ -0,0 +1,124 @@
+% Resolución de Ecuaciones Diferenciales (ED) por medio de Runge-Kutta Heun
+
+% Sección de inicializacion
+% Variable x cambia por variable t, modificar solo si es necesario
+clear all
+clc
+syms t y
+format long
+
+% Introduccion de los datos de trabajo
+% La Ecuacion Diferencial (ED) debe introducirse tal y como la dan en el ejercicio,
+% esto es el modo de trabajo del comando "dsolve"; también se solicita 2 veces la condicion
+% inicial: una para el comando dsolve y la otra para valores numericos para mostrar en la
+% matriz de resolucion
+disp('Resolucion de ED por medio de Heun')
+
+a = input('Introduzca el valor del limite inferior: ');
+b = input('Introduzca el valor del limite superior: ');
+c = input('Introduzca la condicion inicial entre comillas -> y(a)=x: ');
+c1 = input('Introduzca el valor de la condicion inicial: ');
+F = input('Introduzca la Ecuacion Diferencial entre comillas: ');
+F1 = input('Introduzca la funcion de trabajo f(t,y) despejada: ');
+h = input('Introduzca el tamaño de paso h: ');
+N = int32((b-a)/h);
+
+% Llenado de matriz de soluciones de ED
+% matriz(filas,columnas)
+% matriz(arriba/abajo, derecha/izquierda)
+mED = zeros(N,4);
+
+% Asignacion de valores iniciales en la matriz de ED
+% se comienzan con los valores iniciales
+fprintf('t \t\t\t Wi+1 \t\t\t Y(t) \t\t\t Error \n')
+mED(1,1) = a;
+mED(1,2) = c1;
+mED(1,3) = c1;
+mED(1,4) = abs(mED(1,3) - mED(1,2));
+fprintf('%2.15f \t %2.15f \t %2.15f \t %e\n', mED(1,1), mED(1,2),mED(1,3),mED(1,4))
+
+% Se llena la matriz con los datos de paso, el valor aproximado de la ED en base a la
+% valor W(i+1), el valor según la resolucion del dsolve y el error entre los nodos
+f = dsolve(F,c);
+for i=2:N+1
+	mED(i,1) = mED(i-1,1) + h;
+	wiI = mED(i-1,2) + (2*h/3)*subs(F1,[t,y],[mED(i-1,1),mED(i-1,2)]);
+	tiI = mED(i-1,1)+(2*h/3);
+	fun1 = subs(F1,[t,y],[mED(i-1,1),mED(i-1,2)]);
+	fun2 = subs(F1,[t,y],[tiI,wiI]);
+	mED(i,2) = mED(i-1,2) + (h/4)*fun1 + (3*h/4)*fun2;
+	mED(i,3) = subs(f,mED(i,1));
+	mED(i,4) = abs(mED(i,3) - mED(i,2));
+ 	fprintf('%2.15f \t %2.15f \t %2.15f \t %e\n', mED(i,1), mED(i,2),mED(i,3),mED(i,4))
+end
+fprintf('\n');
+
+% El ejercicio puede solicitar interpolacion para un valor en particular, 
+% si es el caso se debe colocar el metodo a ocupar junto con los datos de
+% las matrices de X (mED(:,1)) y Y (mED:,2)
+
+% El comando "menu" permite crear un dialogo con una o varias opciones para 
+% seleccion, este se ocupa como manera interactiva para consultar al usuario
+% si necesita hacer la parte de interpolacion en dicho ejercicio
+choice = menu('¿El ejercicio solicita valor de interpolacion?','Si','No');
+if choice==0 | choice==2
+   break;
+else
+	% Opciones disponibles para resoluciones de aproximacion en ED
+	% LagrangeED: Interpolacion de Lagrange
+	% NevilleED: Interpolacion de Neville
+	% DifDividED: Interpolacion por Diferencias Divididas
+	% HermiteED: Interpolacion por Hermite (principalmente Difer. Dividid.)
+	% TrazadoresED: Interpolacion por Trazadores Cubicos
+	% respuesta = metodo(X,Y,valor a interpolar);
+	% Este cambio debe hacerse dentro de este codigo, ya que no es posible colocarlo
+	% automaticamente, posterior a eso colocar el metodo a ocupar
+	v_interp = input('Introduzca el valor a aproximar: ');
+	resp_interp = LagrangeED(mED(:,1),mED(:,2), v_interp);
+
+	fprintf('El valor de aproximado de la ED en %2.15f es : %2.15f \n\n',v_interp,resp_interp);
+end
+
+
+
+
+
+
+
+
+
+% Metodo de Runge-Kutta modificado de Heun
+% Wi+1 = Wi + (h/4)*f(ti,wi) + (3h/4)*f(ti+(2h/3),wi+(2h/3)*f(ti,wi))
+a = input('Ingrese el limite inferior a: ');
+b = input('Ingrese el limite superior b: ');
+cond_i = input('Ingrese el valor de la condicion inicial: ');
+h = input('Ingrese el tamaño de paso h: ');
+syms t y
+F = input('Ingrese la función de trabajo: ');
+ed = input('Ingrese la ED entre comillas simples: ');
+c = input('Ingrese la condicion inicial entre comillas (y(a)=x): ');
+n = int32(((b-a)/h)+1);
+mx = zeros(3,n);
+mx(1,1) = a;
+mx(1,n) = b;
+% Estableciendo los ti a partir del limite inferior y el valor de h
+for j=2:n-1
+    mx(1,j) = mx(1,j-1) + h;
+end
+% Asignando el valor de la condicion inicial al primer valor aproximado y
+% exacto
+mx(2,1) = cond_i;
+mx(3,1) = cond_i;
+% Encontrando la solucion de la ED ordinaria con ayuda de la condicion
+% inicial
+s = dsolve(ed,c);
+for j=2:n
+    wi_interior = mx(2,j-1)+(2/3)*h*subs(F,[t,y],[mx(1,j-1),mx(2,j-1)]); % »» wi+(2h/3)*f(ti,wi)
+    mx(2,j) = mx(2,j-1)+(h/4)*subs(F,[t,y],[mx(1,j-1),mx(2,j-1)])+(3/4)*h*subs(F,[t,y],[mx(1,j-1)+(2/3)*h,wi_interior]);
+    mx(3,j) = subs(s,mx(1,j));
+end
+% Imprimiendo datos
+fprintf('\n       ti               Wi+1               y(t)\n');
+for j=1:n
+    fprintf('%9.15f %9.15f %9.15f\n\n',mx(1,j),mx(2,j),mx(3,j));
+end
